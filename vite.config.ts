6a447ce0783cfee135c0a2b9bc78d105cceed168
Matthias <voxel@vivaldi.net>
2026-02-05 23:39:14 +0100

    split packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'framer-motion'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'datagrid-vendor': ['@mui/x-data-grid'],
+          'recharts-vendor': ['recharts'],
+          'pocketbase': ['pocketbase'],
+          'utils-vendor': ['date-fns']
+        }
+      }
+    }
+  }
 })
